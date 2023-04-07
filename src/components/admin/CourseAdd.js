b5ca3224f0449df6231aab74f969c1c3ab39d48c
Judyy22 <115631038+Judyy22@users.noreply.github.com>
2023-04-07 15:46:20 +0900

    프로그램등록 양식 만듦

--- src/components/admin/CourseAdd.js
+++ src/components/admin/CourseAdd.js
@@ -3,6 +3,11 @@ import { Container, Form } from "react-bootstrap";
 import "./Admin.css";
 
 const CourseAdd = () => {
+    const preventKey = (e) => {
+        if (e.key === "Enter") {
+            e.preventDefault();
+        }
+    };
     const handleSubmit = (event) => {
         event.preventDefault();
         const week = [
@@ -19,37 +24,50 @@ const CourseAdd = () => {
     };
 
     return (
-        <Form onSubmit={handleSubmit} className="CourseAddForm">
+        <Form
+            onSubmit={handleSubmit}
+            onKeyPress={preventKey}
+            className="CourseAddForm"
+        >
             <div>
                 <div>프로그램명</div>
-                <input type="text" id="name" />
+                <input type="text" id="name" placeholder="프로그램 이름" />
+            </div>
+            <div>
+                <div>프로그램 종류</div>
+                <div className="checkBoxType">
+                    <input type="checkbox" name="type" value="edu" />
+                    <label>교육</label>
+                    <input type="checkbox" name="type" value="exp" />
+                    <label>체험</label>
+                </div>
             </div>
             <div>
                 <div>수강료</div>
-                <input type="number" id="fee" />
+                <input type="number" id="fee" placeholder="수강료" />
             </div>
             <div>
                 <div>수강인원</div>
-                <input type="number" id="limit" />
+                <input type="number" id="limit" placeholder="수강 인원" />
             </div>
             <div className="startTime">
-                <div>교육 시작일</div>
+                <div>프로그램 시작일</div>
                 <input type="date" id="startdate" />
             </div>
             <div className="endTime">
-                <div>교육 종료일</div>
+                <div>프로그램 종료일</div>
                 <input type="date" id="enddate" />
             </div>
             <div className="startTime">
-                <div>교육 시작시간</div>
+                <div>프로그램 시작시간</div>
                 <input type="time" id="starttime" />
             </div>
             <div className="endTime">
-                <div>교육 종료시간</div>
+                <div>프로그램 종료시간</div>
                 <input type="time" id="endtime" />
             </div>
             <div>
-                <div>교육 요일</div>
+                <div>프로그램 요일</div>
                 <div className="checkBoxDay">
                     <input type="checkbox" id="week" value="mon" />
                     <label>월</label>
@@ -68,21 +86,21 @@ const CourseAdd = () => {
                 </div>
             </div>
             <div>
-                <div>교육장소</div>
-                <input type="text" id="place" />
+                <div>프로그램 장소</div>
+                <input type="text" id="place" placeholder="교육 장소" />
             </div>
             <div>
                 <div>신청 마감일</div>
                 <input type="date" id="deadline" />
             </div>
+
             <div>
-                <div>강의 종류</div>
-                <div className="checkBoxType">
-                    <input type="checkbox" name="type" value="edu" />
-                    <label>교육</label>
-                    <input type="checkbox" name="type" value="exp" />
-                    <label>체험</label>
-                </div>
+                <div>교육내용</div>
+                <input
+                    type="text"
+                    id="sumary"
+                    placeholder="교육내용 한줄요약"
+                />
             </div>
             <div>
                 <div>전달사항</div>
@@ -90,6 +108,7 @@ const CourseAdd = () => {
                     type="textarea"
                     id="notice"
                     className="CourseAddNotice"
+                    placeholder="안내사항"
                 />
             </div>
             <button type="submit">저장</button>
